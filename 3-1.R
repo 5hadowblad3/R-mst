fe94476ed1a4b0c25b9eeea9aceaa9e61655ff27
Yifanny <zyfjane@live.com>
2015-05-27 01:31:06 +0200

    add comment

--- 3-1.R
+++ 3-1.R
@@ -2,19 +2,23 @@
 library(Matrix)
 library(expm)
 
+#c'est une fonction auxiliaire qui est pour effectuer operation de chaque ligne
 fun<-function(X){
 x0<-c(100,0,0,0,0)
 	return (X*x0)
 }
 
+#cette fonction prend en argument un vecteur de temps d'observation T et un vecteur de parametres theta
+#renvoye la valeur de X(theta) qui represente la masse de chaque concentration a ti 
 apinene_modele_prediction<-function(T,theta){
+	#calcule la matrice theta
 	A<-matrix(c((-theta[1]-theta[2]),0,0,0,0,theta[1],0,0,0,0,theta[2],0,(-theta[3]-theta[4]),0,theta[5],0,0,theta[3],0,0,0,0,theta[4],0,-theta[5]), ncol=5,byrow=TRUE)
 	l<-length(T)
 	res<-matrix(0,nrow=l,ncol=5)
 	res[1,]<-c(100,0,0,0,0)
 	for(i in 2:l){
 		t=T[i]
-		X<-expm(A*t)
+		X<-expm(A*t)                   # X = x0 * exp(A(theta)*t)
 		tmp<-apply(X,1,fun)
 		tmp<-t(tmp)
 		res[i,]<-apply(tmp,1,sum)
